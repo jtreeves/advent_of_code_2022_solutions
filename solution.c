90a3d4248b1b8bc2f531beddef8ca0df764897de
jtreeves <jackson.t.reeves@gmail.com>
2022-12-01 16:25:12 -0500

    [c] Read data from file and print to screen

--- solution.c
+++ solution.c
@@ -1,3 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+int main() {
+    FILE* ptr;
+    char ch;
+ 
+    ptr = fopen("data.txt", "r");
+ 
+    do {
+        ch = fgetc(ptr);
+        printf("%c", ch);
+    } while (ch != EOF);
+ 
+    fclose(ptr);
+    
+    return 0;
+}
