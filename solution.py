f050d7446b698ec0777caed6992cbf80909d0c61
jtreeves <jackson.t.reeves@gmail.com>
2022-12-01 14:35:15 -0500

    [py] Create strings for each elf

--- solution.py
+++ solution.py
@@ -1,5 +1,14 @@
 file = open("data.txt", "r")
-print("BEGINNING FILE")
-print(file.read())
+read_file = file.read()
+blocks = []
+
+def create_strings_for_each_elf(current_file):
+    while len(current_file) > 4:
+        find_first = current_file.find("\n\n")
+        file_before = current_file[0:find_first]
+        blocks.append(file_before)
+        current_file = current_file[find_first+2:]
+
+create_strings_for_each_elf(read_file)
+print(blocks)
 file.close()
-print("ENDING FILE")
