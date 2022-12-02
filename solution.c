8b84ae5c818f27fd6050728aef112ea82779a1a0
jtreeves <jackson.t.reeves@gmail.com>
2022-12-02 17:06:11 -0500

    [c] Attempt to change printing to storing

--- solution.c
+++ solution.c
@@ -5,14 +5,17 @@
 int main() {
     FILE* ptr;
     char ch;
+    char file_contents[10000000] = "";
  
     ptr = fopen("data.txt", "r");
  
     do {
         ch = fgetc(ptr);
-        printf("%c", ch);
+        strncat(file_contents, &ch, 1);
+        // printf("%c", ch);
     } while (ch != EOF);
  
+    printf("%s", file_contents);
     fclose(ptr);
     
     return 0;
