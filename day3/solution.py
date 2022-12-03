7a24e1bbe523b235c7508bdb20dcedc2ccb411f7
jtreeves <jackson.t.reeves@gmail.com>
2022-12-03 00:20:54 -0500

    [py] Split items in half

--- day3/solution.py
+++ day3/solution.py
@@ -1,7 +1,15 @@
+def split_items_in_half(items):
+    length = len(items)
+    half = int(length / 2)
+    first_half = items[0:half]
+    second_half = items[half:]
+    return [first_half, second_half]
+
 def convert_letter_to_priority(letter):
     letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     index = letters.index(letter)
     priority = index + 1
     return priority
 
-print(convert_letter_to_priority("A"))
+# print(convert_letter_to_priority("A"))
+print(split_items_in_half("uxjeYYednJ"))
