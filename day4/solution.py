c3b3bc5e011c12da17bb58fc063fdf98ebe8f6b8
jtreeves <jackson.t.reeves@gmail.com>
2022-12-04 00:15:09 -0500

    [py] Convert strings to ints when determining endpoints

--- day4/solution.py
+++ day4/solution.py
@@ -1,4 +1,4 @@
-def determine_if_interval_contains_other_interval(first_interval, second_interval):
+def determine_if_one_interval_contains_other_interval(first_interval, second_interval):
     if (first_interval[0] <= second_interval[0] and first_interval[1] >= second_interval[1]) or (second_interval[0] <= first_interval[0] and second_interval[1] >= first_interval[1]):
         return True
     else:
@@ -8,10 +8,15 @@ def split_apart_assignments_from_pair(pair):
     assignments = pair.split(",")
     return assignments
 
-def determine_endpoints_of_assignment_range(assignment):
-    endpoints = assignment.split("-")
+def find_endpoints_of_assignment_range(assignment):
+    endpoints = []
+    string_values = assignment.split("-")
+    for value in string_values:
+        endpoints.append(int(value))
     return endpoints
 
-print(determine_if_interval_contains_other_interval([2, 4], [6, 8]))
-print(determine_if_interval_contains_other_interval([2, 8], [3, 7]))
-print(determine_if_interval_contains_other_interval([6, 6], [4, 6]))
+print(determine_if_one_interval_contains_other_interval([2, 4], [6, 8]))
+print(determine_if_one_interval_contains_other_interval([2, 8], [3, 7]))
+print(determine_if_one_interval_contains_other_interval([6, 6], [4, 6]))
+print(find_endpoints_of_assignment_range("2-8"))
+print(find_endpoints_of_assignment_range("172-893"))
