bc2e7f25e8aedc83ba356b4c6207c994b2145287
jtreeves <jackson.t.reeves@gmail.com>
2022-12-04 00:21:26 -0500

    [py] Calculate total across all pairs

--- day4/solution.py
+++ day4/solution.py
@@ -1,3 +1,16 @@
+def calculate_total_of_all_complete_overlaps(pairs):
+    total = 0
+    for pair in pairs:
+        assignments = split_apart_assignments_from_pair(pair)
+        first_assignment = assignments[0]
+        second_assignment = assignments[1]
+        first_interval = find_endpoints_of_assignment_range(first_assignment)
+        second_interval = find_endpoints_of_assignment_range(second_assignment)
+        overlap = determine_if_one_interval_contains_other_interval(first_interval, second_interval)
+        if overlap:
+            total += 1
+    return total
+
 def determine_if_one_interval_contains_other_interval(first_interval, second_interval):
     if (first_interval[0] <= second_interval[0] and first_interval[1] >= second_interval[1]) or (second_interval[0] <= first_interval[0] and second_interval[1] >= first_interval[1]):
         return True
@@ -15,8 +28,9 @@ def find_endpoints_of_assignment_range(assignment):
         endpoints.append(int(value))
     return endpoints
 
-print(determine_if_one_interval_contains_other_interval([2, 4], [6, 8]))
-print(determine_if_one_interval_contains_other_interval([2, 8], [3, 7]))
-print(determine_if_one_interval_contains_other_interval([6, 6], [4, 6]))
-print(find_endpoints_of_assignment_range("2-8"))
-print(find_endpoints_of_assignment_range("172-893"))
+# print(determine_if_one_interval_contains_other_interval([2, 4], [6, 8]))
+# print(determine_if_one_interval_contains_other_interval([2, 8], [3, 7]))
+# print(determine_if_one_interval_contains_other_interval([6, 6], [4, 6]))
+# print(find_endpoints_of_assignment_range("2-8"))
+# print(find_endpoints_of_assignment_range("172-893"))
+print(calculate_total_of_all_complete_overlaps(["2-4,6-8","2-3,4-5","5-7,7-9","2-8,3-7","6-6,4-6","2-6,4-8"]))
