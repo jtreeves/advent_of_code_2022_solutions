b9b00e7cc32467b5ef512ee948ef3690c541e4cf
jtreeves <jackson.t.reeves@gmail.com>
2022-12-10 16:49:20 -0500

    [py] Check if line is directory or command

---
+++ day7/solution.py
@@ -0,0 +1,31 @@
+def check_if_command(line):
+    first_character = line[0]
+    is_command = first_character == "$"
+    return is_command
+
+def check_if_directory(line):
+    first_characters = line[0:3]
+    is_directory = first_characters == "dir"
+    return is_directory
+
+def convert_multiline_string_to_array(multiline_string):
+    rows = []
+    while len(multiline_string):
+        first_line_break = multiline_string.find("\n")
+        if first_line_break != -1:
+            content_before = multiline_string[0:first_line_break]
+            rows.append(content_before)
+            multiline_string = multiline_string[first_line_break+1:]
+        else:
+            rows.append(multiline_string)
+            multiline_string = ""
+    return rows
+
+def extract_data_from_file(day_number):
+    file = open(f"day{day_number}/data.txt", "r")
+    data = file.read()
+    file.close()
+    return data
+
+print(check_if_command("$ ls"))
+print(check_if_directory("234 a"))
