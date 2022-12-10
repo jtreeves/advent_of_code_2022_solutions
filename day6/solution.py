089b6b9b2ed3360a539d88ab248a5cc36bf9c427
jtreeves <jackson.t.reeves@gmail.com>
2022-12-10 15:44:35 -0500

    [py] Find first marker after a string of 4 unique characters

--- day6/solution.py
+++ day6/solution.py
@@ -1,3 +1,14 @@
+def find_first_marker_after_uniques(characters):
+    marker = 4
+    first_four_characters = extract_first_four_characters(characters)
+    checked_characters = check_no_characters_repeat(first_four_characters)
+    while not checked_characters:
+        marker += 1
+        characters = characters[1:]
+        first_four_characters = extract_first_four_characters(characters)
+        checked_characters = check_no_characters_repeat(first_four_characters)
+    return marker
+
 def extract_first_four_characters(characters):
     return characters[0:4]
 
@@ -15,4 +26,9 @@ def extract_data_from_file(day_number):
     return data
 
 # print(check_no_characters_repeat("abcb"))
-print(extract_first_four_characters("mjqjpqmgbljsphdztnvjfqwrcgsmlb"))
+# print(extract_first_four_characters("mjqjpqmgbljsphdztnvjfqwrcgsmlb"))
+print(find_first_marker_after_uniques("mjqjpqmgbljsphdztnvjfqwrcgsmlb"))
+print(find_first_marker_after_uniques("bvwbjplbgvbhsrlpgdmjqwftvncz"))
+print(find_first_marker_after_uniques("nppdvjthqldpwncqszvftbrmjlhg"))
+print(find_first_marker_after_uniques("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"))
+print(find_first_marker_after_uniques("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))
