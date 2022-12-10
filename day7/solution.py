534b38a7e43034e06213b549d7e2740e600792dc
jtreeves <jackson.t.reeves@gmail.com>
2022-12-10 17:35:07 -0500

    [py] Convert directory array to object

--- day7/solution.py
+++ day7/solution.py
@@ -6,6 +6,32 @@ def extract_contents_of_directories(lines):
         contents.append(section_array)
     return contents
 
+def convert_directory_array_to_object(directory_array):
+    directory_object = {}
+    for element in directory_array:
+        if not check_if_command(element):
+            if not check_if_directory(element):
+                file = get_file_name_and_size(element)
+                directory_object[file["name"]] = file["size"]
+            else:
+                new_directory = get_directory_name(element)
+                directory_object[new_directory] = {}
+    return directory_object
+
+def get_directory_name(line):
+    partition = line.split(" ")
+    name = partition[1]
+    return name
+
+def get_file_name_and_size(line):
+    partition = line.split(" ")
+    name = partition[1]
+    size = partition[0]
+    return {
+        "name": name,
+        "size": size
+    }
+
 def check_if_command(line):
     first_character = line[0]
     is_command = first_character == "$"
@@ -40,5 +66,6 @@ def extract_data_from_file(day_number):
 
 # print(check_if_command("$ ls"))
 # print(check_if_directory("234 a"))
-print(extract_contents_of_directories("$ cd /\n$ ls\ndir a\n14848514 b.txt\n8504156 c.dat\ndir d\n$ cd a\n$ ls\ndir e\n29116 f\n2557 g\n62596 h.lst\n$ cd e\n$ ls\n584 i\n$ cd ..\n$ cd ..\n$ cd d\n$ ls\n4060174 j\n8033020 d.log\n5626152 d.ext\n7214296 k"))
-print(determine_directory_name("$ cd bcd"))
+# print(extract_contents_of_directories("$ cd /\n$ ls\ndir a\n14848514 b.txt\n8504156 c.dat\ndir d\n$ cd a\n$ ls\ndir e\n29116 f\n2557 g\n62596 h.lst\n$ cd e\n$ ls\n584 i\n$ cd ..\n$ cd ..\n$ cd d\n$ ls\n4060174 j\n8033020 d.log\n5626152 d.ext\n7214296 k"))
+# print(determine_directory_name("$ cd bcd"))
+print(convert_directory_array_to_object(['dir e', '29116 f', '2557 g', '62596 h.lst', '$ cd e']))
