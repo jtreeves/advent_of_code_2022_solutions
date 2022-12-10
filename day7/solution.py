840e3108ec294bbd3f095dd2b6787507d63ef882
jtreeves <jackson.t.reeves@gmail.com>
2022-12-10 17:20:08 -0500

    [py] Determine directory name

--- day7/solution.py
+++ day7/solution.py
@@ -1,3 +1,11 @@
+def extract_contents_of_directories(lines):
+    sections = lines.split("$ ls\n")
+    contents = []
+    for section in sections:
+        section_array = convert_multiline_string_to_array(section)
+        contents.append(section_array)
+    return contents
+
 def check_if_command(line):
     first_character = line[0]
     is_command = first_character == "$"
@@ -7,6 +15,9 @@ def check_if_directory(line):
     first_characters = line[0:3]
     is_directory = first_characters == "dir"
     return is_directory
+
+def determine_directory_name(line):
+    return line[5:]
 
 def convert_multiline_string_to_array(multiline_string):
     rows = []
@@ -27,5 +38,7 @@ def extract_data_from_file(day_number):
     file.close()
     return data
 
-print(check_if_command("$ ls"))
-print(check_if_directory("234 a"))
+# print(check_if_command("$ ls"))
+# print(check_if_directory("234 a"))
+print(extract_contents_of_directories("$ cd /\n$ ls\ndir a\n14848514 b.txt\n8504156 c.dat\ndir d\n$ cd a\n$ ls\ndir e\n29116 f\n2557 g\n62596 h.lst\n$ cd e\n$ ls\n584 i\n$ cd ..\n$ cd ..\n$ cd d\n$ ls\n4060174 j\n8033020 d.log\n5626152 d.ext\n7214296 k"))
+print(determine_directory_name("$ cd bcd"))
