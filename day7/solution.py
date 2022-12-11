d4ab738d9513cd5b49883fb05dd686472aea547a
jtreeves <jackson.t.reeves@gmail.com>
2022-12-10 20:11:42 -0500

    [py] Find correct directory in an array

--- day7/solution.py
+++ day7/solution.py
@@ -15,13 +15,18 @@ def extract_contents_of_directories(lines):
         folders.append({
             next_directory_name: next_directory_contents
         })
-    result = {}
-    for folder in folders:
-        folder_name = list(folder.keys())[0]
-        result_keys = result.keys()
-        if len(result_keys) == 0:
-            result[folder_name] = folder[folder_name]
+    result = folders[0]
+    result_keys = result.keys()
+    for key in result_keys:
+        key_values = result[key]
+        is_integer = check_if_object_property_is_integer(result, key)
     return result
+
+def find_correct_directory_in_array(directories, name):
+    for directory in directories:
+        directory_name = list(directory.keys())[0]
+        if directory_name == name:
+            return directory
 
 def find_key_in_nested_objects(nest, key):
     path = ""
@@ -107,4 +112,5 @@ def extract_data_from_file(day_number):
 # print(check_if_directory("234 a"))
 # print(extract_contents_of_directories("$ cd /\n$ ls\ndir a\n14848514 b.txt\n8504156 c.dat\ndir d\n$ cd a\n$ ls\ndir e\n29116 f\n2557 g\n62596 h.lst\n$ cd e\n$ ls\n584 i\n$ cd ..\n$ cd ..\n$ cd d\n$ ls\n4060174 j\n8033020 d.log\n5626152 d.ext\n7214296 k"))
 # print(convert_directory_array_to_object(['dir e', '29116 f', '2557 g', '62596 h.lst', '$ cd e']))
-print(find_key_in_nested_objects({'a': 1}, 'a'))
+# print(find_key_in_nested_objects({'a': 1}, 'a'))
+print(find_correct_directory_in_array([{'a': {}}, {'b': {}}], 'a'))
