9827a872a7e00758daffede1f9e15bb79de44550
jtreeves <jackson.t.reeves@gmail.com>
2022-12-11 20:40:12 -0500

    [py] Determine register increment

--- day10/solution.py
+++ day10/solution.py
@@ -1,9 +1,28 @@
+def determine_register_statuses_at_key_cycles(moves):
+    cycles = list_all_cycle_increments(moves)
+    starting_values = {
+        "status": 1,
+        "cycles": 1,
+    }
+    key_values = [starting_values]
+    for index in range(len(cycles)):
+        new_status = key_values[index]
+
 def list_all_cycle_increments(moves):
     cylces = []
     for move in moves:
         cycle = determine_cycles_to_wait(move)
         cylces.append(cycle)
     return cylces
+
+def determine_register_increment(move):
+    is_noop = move == "noop"
+    if is_noop:
+        return 0
+    else:
+        partitioned = move.split(" ")
+        increment = int(partitioned[1])
+        return increment
 
 def determine_cycles_to_wait(move):
     is_noop = move == "noop"
