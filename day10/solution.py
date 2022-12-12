7cbd7db50986008aff41a1460c0577dbe30a5b0a
jtreeves <jackson.t.reeves@gmail.com>
2022-12-11 20:24:50 -0500

    [py] Determine cycles to wait

--- day10/solution.py
+++ day10/solution.py
@@ -1,4 +1,13 @@
+def determine_cycles_to_wait(move):
+    is_noop = move == "noop"
+    if is_noop:
+        return 1
+    else:
+        return 2
+
 def list_all_moves(instructions):
     moves = instructions.split("\n")
     return moves
-    
+
+print(determine_cycles_to_wait("noop"))
+print(determine_cycles_to_wait("addx -6"))
