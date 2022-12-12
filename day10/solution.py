a5d75a700c17a4645e71cfccf052f44b6fc486a3
jtreeves <jackson.t.reeves@gmail.com>
2022-12-11 20:27:01 -0500

    [py] List all cycle increments

--- day10/solution.py
+++ day10/solution.py
@@ -1,3 +1,10 @@
+def list_all_cycle_increments(moves):
+    cylces = []
+    for move in moves:
+        cycle = determine_cycles_to_wait(move)
+        cylces.append(cycle)
+    return cylces
+
 def determine_cycles_to_wait(move):
     is_noop = move == "noop"
     if is_noop:
@@ -9,5 +16,6 @@ def list_all_moves(instructions):
     moves = instructions.split("\n")
     return moves
 
-print(determine_cycles_to_wait("noop"))
-print(determine_cycles_to_wait("addx -6"))
+# print(determine_cycles_to_wait("noop"))
+# print(determine_cycles_to_wait("addx -6"))
+print(list_all_cycle_increments(['noop', 'addx 5', 'noop', 'addx 1', 'addx -2']))
