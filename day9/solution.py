cd1201af301959da4a1024bac811b12a324585fa
jtreeves <jackson.t.reeves@gmail.com>
2022-12-14 16:40:48 -0500

    [py] Use tuple and set global variables for tail position

--- day9/solution.py
+++ day9/solution.py
@@ -1,10 +1,22 @@
+tail_current_position = tuple()
 tail_positions = set()
 
 def add_new_tail_position(position):
     tail_positions.add(position)
+
+def change_tail_current_position(new_position):
+    tail_current_position = new_position
+    add_new_tail_position(tail_current_position)
 
 def extract_data_from_file(day_number):
     file = open(f"day{day_number}/data.txt", "r")
     data = file.read()
     file.close()
     return data
+
+change_tail_current_position((1, 1))
+change_tail_current_position((2, 3))
+change_tail_current_position((5, 10))
+change_tail_current_position((7, 7))
+change_tail_current_position((5, 10))
+print(tail_positions)
