7b9fd9f89b22d4de17a8cae5f796038e6cdc66b1
jtreeves <jackson.t.reeves@gmail.com>
2022-12-15 19:03:44 -0500

    [py] Create formatted packet pairs; return list of all pairs in main solve

--- day13/solution.py
+++ day13/solution.py
@@ -1,5 +1,28 @@
 import json
 
+def solve_problem():
+    data = extract_data_from_file(13)
+    pairs = list_all_formatted_packet_pairs(data)
+    return pairs
+
+def list_all_formatted_packet_pairs(data):
+    raw_pairs = list_all_raw_pairs(data)
+    all_pairs = []
+    for pair in raw_pairs:
+        formatted_pair = create_formatted_packet_pair(pair)
+        all_pairs.append(formatted_pair)
+    return all_pairs
+
+def create_formatted_packet_pair(original_pair):
+    raw_packets = separate_raw_packets(original_pair)
+    left = convert_string_to_list(raw_packets[0])
+    right = convert_string_to_list(raw_packets[1])
+    formatted_pair = {
+        "left": left,
+        "right": right
+    }
+    return formatted_pair
+
 def convert_string_to_list(input):
     output = json.loads(input)
     return output
@@ -17,3 +40,6 @@ def extract_data_from_file(day_number):
     data = file.read()
     file.close()
     return data
+
+result = solve_problem()
+print(result)
