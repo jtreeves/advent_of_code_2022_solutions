d3d5ea14399ee3339f361f0bb2093c9db59ab04a
jtreeves <jackson.t.reeves@gmail.com>
2022-12-18 17:34:30 -0500

    [py] Add left_border, right_border, and bottom_border properties to rocks; add floor to chamber

--- day17/solution.py
+++ day17/solution.py
@@ -24,12 +24,22 @@ class Chamber:
         self.current_iteration = 0
         self.next_jet_blow = self.jet_pattern[self.current_iteration]
         self.height = 0
+        self.full_line_height = None
+        self.floor = {}
+        for i in range(7):
+            self.floor[i + 1] = 0
     
     def drop_new_rock(self):
         new_rock = Rock(self.next_rock_type, self.height)
         while not new_rock.touching_rock_or_floor(self.spaces):
             self.increment_pattern(new_rock)
             new_rock.fall_down(self.spaces)
+            for rock in new_rock.left_border:
+                print(f"LEFT ROCK: [{rock.x}, {rock.y}]")
+            for rock in new_rock.right_border:
+                print(f"RIGHT ROCK: [{rock.x}, {rock.y}]")
+            for rock in new_rock.bottom_border:
+                print(f"BOTTOM ROCK: [{rock.x}, {rock.y}]")
         for point in new_rock.shape:
             self.spaces.append(point)
         self.increment_new_rock_type()
@@ -53,6 +63,9 @@ class Chamber:
             self.next_rock_type += 1
         else:
             self.next_rock_type = 1
+    
+    def increment_height(self):
+        return
 
 class Rock:
     def __init__(self, type, height):
@@ -60,6 +73,9 @@ class Rock:
             self.shape = []
             for i in range(4):
                 self.shape.append(Coordinate(i + 3, height + 4))
+            self.left_border = [self.shape[0]]
+            self.right_border = [self.shape[3]]
+            self.bottom_border = self.shape
         elif type == 2:
             self.shape = [
                 Coordinate(4, height + 3),
@@ -67,6 +83,21 @@ class Rock:
                 Coordinate(4, height + 4),
                 Coordinate(5, height + 4),
                 Coordinate(4, height + 5),
+            ]
+            self.left_border = [
+                self.shape[0],
+                self.shape[1],
+                self.shape[4],
+            ]
+            self.right_border = [
+                self.shape[0],
+                self.shape[3],
+                self.shape[4],
+            ]
+            self.bottom_border = [
+                self.shape[0],
+                self.shape[1],
+                self.shape[3],
             ]
         elif type == 3:
             self.shape = [
@@ -76,10 +107,16 @@ class Rock:
                 Coordinate(5, height + 4),
                 Coordinate(5, height + 5),
             ]
+            self.left_border = [self.shape[0]]
+            self.right_border = [self.shape[2:]]
+            self.bottom_border = [self.shape[:3]]
         elif type == 4:
             self.shape = []
             for i in range(4):
                 self.shape.append(Coordinate(3, height + i + 3))
+            self.left_border = self.shape
+            self.right_border = self.shape
+            self.bottom_border = [self.shape[0]]
         else:
             self.shape = [
                 Coordinate(3, height + 3),
@@ -87,6 +124,15 @@ class Rock:
                 Coordinate(3, height + 4),
                 Coordinate(4, height + 4),
             ]
+            self.left_border = [
+                self.shape[0],
+                self.shape[2]
+            ]
+            self.right_border = [
+                self.shape[1],
+                self.shape[3]
+            ]
+            self.bottom_border = self.shape[:2]
 
     def blown_left(self, other_points):
         if not self.touching_left_wall() and not self.touching_rock_or_floor(other_points):
@@ -104,13 +150,13 @@ class Rock:
                 point.move_down()
 
     def touching_left_wall(self):
-        for point in self.shape:
+        for point in self.left_border:
             if point.x == 1:
                 return True
         return False
 
     def touching_right_wall(self):
-        for point in self.shape:
+        for point in self.right_border:
             if point.x == 7:
                 return True
         return False
@@ -125,7 +171,8 @@ class Rock:
 def solve_problem():
     data = extract_data_from_file(17)
     chamber = Chamber(data)
-    for i in range(3):
+    print(chamber.floor)
+    for _ in range(1):
         chamber.drop_new_rock()
     for space in chamber.spaces:
         print([space.x, space.y])
