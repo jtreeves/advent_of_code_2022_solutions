9276a7af286db4b6ad6c0189409e06f27324c415
jtreeves <jackson.t.reeves@gmail.com>
2022-12-18 22:37:42 -0500

    [py] Use itertools module and zip to find all vertices based on corner and diagonal

--- day18/solution.py
+++ day18/solution.py
@@ -1,6 +1,10 @@
+import itertools
+
 def solve_problem():
     data = extract_data_from_file(18)
-    return data
+    instructions = list_all_cube_initial_coordinates(data)
+    cubes = generate_all_cubes(instructions)
+    return cubes
 
 def generate_all_cubes(instructions):
     cubes = []
@@ -11,23 +15,17 @@ def generate_all_cubes(instructions):
     return cubes
 
 def generate_all_vertices(corner):
-    x = corner[0]
-    y = corner[1]
-    z = corner[2]
-    x1 = x + 1
-    y1 = y + 1
-    z1 = z + 1
-    vertices = [
-        [x, y, z],
-        [x1, y1, z1],
-        [x, y, z1],
-        [x, y1, z],
-        [x1, y, z],
-        [x, y1, z1],
-        [x1, y, z1],
-        [x1, y1, z],
-    ]
+    diagonal = []
+    for coordinate in corner:
+        diagonal.append(coordinate + 1)
+    vertices = list(itertools.product(*zip(corner, diagonal)))
     return vertices
+
+# def generate_all_sides(corner):
+#     sides = []
+#     for coordinate in corner:
+
+#     return sides
 
 def convert_string_coordinates(string_coordinates):
     coordinates = string_coordinates.split(",")
