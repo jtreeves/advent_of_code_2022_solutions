69ed27b662adf860e14e7744fed68e9ead15c6c1
jtreeves <jackson.t.reeves@gmail.com>
2022-12-20 00:15:32 -0500

    [20] Extract data; list numbers; make copy of list

--- day12/solution.py
+++ day12/solution.py
@@ -1,3 +1,21 @@
+class Cell:
+    def __init__(self, x, y, value):
+        self.x = x
+        self.y = y
+        self.value = value
+
+class Grid:
+    def __init__(self, description):
+        self.description = description.split("\n")
+        self.height = self.calculate_height()
+        self.width = self.calculate_width()
+    
+    def calculate_height(self):
+        return len(self.description)
+    
+    def calculate_width(self):
+        return len(self.description[0])
+
 def convert_letter_to_number(letter):
     if letter == "S":
         return 1
---
+++ day20/solution.py
@@ -0,0 +1,31 @@
+print(1004 % 7)
+print(2004 % 7)
+print(3004 % 7)
+
+def solve_problem():
+    data = extract_data_from_file(20)
+    numbers = list_all_numbers(data)
+    return numbers
+
+def copy_original_list(original_list):
+    copy = []
+    for element in original_list:
+        copy.append(element)
+    return copy
+
+def list_all_numbers(data):
+    string_numbers = data.split("\n")
+    integer_numbers = []
+    for string in string_numbers:
+        integer = int(string)
+        integer_numbers.append(integer)
+    return integer_numbers
+
+def extract_data_from_file(day_number):
+    file = open(f"day{day_number}/data.txt", "r")
+    data = file.read()
+    file.close()
+    return data
+
+result = solve_problem()
+print(result)
