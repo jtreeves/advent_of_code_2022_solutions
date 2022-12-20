26e0ce8aa26efe51d655f79a8c2cb032b133cdc7
jtreeves <jackson.t.reeves@gmail.com>
2022-12-20 00:23:48 -0500

    [20] Determine values at key indices after 0

--- day20/solution.py
+++ day20/solution.py
@@ -6,7 +6,27 @@ def solve_problem():
     data = extract_data_from_file(20)
     numbers = list_all_numbers(data)
     index = find_index_of_zero(numbers)
-    return index
+    mixed_list = [1, 2, -3, 4, 0, 3, -2]
+    key_values = determine_values_at_key_indices_after_zero(mixed_list)
+    return key_values
+
+def determine_values_at_key_indices_after_zero(mixed_list):
+    length = len(mixed_list)
+    index = find_index_of_zero(mixed_list)
+    first_index = index + 1000
+    second_index = first_index + 1000
+    third_index = second_index + 1000
+    first_modulo = first_index % length
+    second_modulo = second_index % length
+    third_modulo = third_index % length
+    first_value = mixed_list[first_modulo]
+    second_value = mixed_list[second_modulo]
+    third_value = mixed_list[third_modulo]
+    return [
+        first_value,
+        second_value,
+        third_value
+    ]
 
 def find_index_of_zero(mixed_list):
     index = mixed_list.index(0)
