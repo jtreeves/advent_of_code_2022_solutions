9643ca73feffeb72fdbf128c47913c6b523f5d1d
jtreeves <jackson.t.reeves@gmail.com>
2022-12-20 02:05:39 -0500

    [20] Adjust other helpers to deal with objects instead of integers

--- day20/solution.py
+++ day20/solution.py
@@ -2,7 +2,8 @@ def solve_problem():
     data = extract_data_from_file(20)
     numbers = list_all_numbers(data)
     numbers = mix_list(numbers)
-    return numbers
+    summed = sum_key_values(numbers)
+    return summed
 
 def sum_key_values(mixed_list):
     key_values = determine_values_at_key_indices_after_zero(mixed_list)
@@ -20,9 +21,9 @@ def determine_values_at_key_indices_after_zero(mixed_list):
     first_modulo = first_index % length
     second_modulo = second_index % length
     third_modulo = third_index % length
-    first_value = mixed_list[first_modulo]
-    second_value = mixed_list[second_modulo]
-    third_value = mixed_list[third_modulo]
+    first_value = mixed_list[first_modulo]["value"]
+    second_value = mixed_list[second_modulo]["value"]
+    third_value = mixed_list[third_modulo]["value"]
     return [
         first_value,
         second_value,
@@ -30,8 +31,9 @@ def determine_values_at_key_indices_after_zero(mixed_list):
     ]
 
 def find_index_of_zero(mixed_list):
-    index = mixed_list.index(0)
-    return index
+    for i in range(len(mixed_list)):
+        if mixed_list[i]["value"] == 0:
+            return i
 
 def mix_list(list):
     for i in range(len(list)):
