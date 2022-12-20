76b0688bf77312d27705a5058f90c86097e42408
jtreeves <jackson.t.reeves@gmail.com>
2022-12-20 01:09:45 -0500

    [20] Store objects instead of integers in list; add helper to find current index based on original index

--- day20/solution.py
+++ day20/solution.py
@@ -1,15 +1,15 @@
 def solve_problem():
     data = extract_data_from_file(20)
     numbers = list_all_numbers(data)
-    updated_list = move_element(1, 0, numbers)
-    updated_list = move_element(2, 0, updated_list)
-    updated_list = move_element(-3, 1, updated_list)
+    move_element(1, 0, 0, numbers)
+    move_element(2, 0, 1, numbers)
+    move_element(-3, 1, 2, numbers)
     # index = find_index_of_zero(numbers)
     # mixed_list = [1, 2, -3, 4, 0, 3, -2]
     # total = sum_key_values(mixed_list)
     # unique = check_unique(numbers)
-    print(f"ORIGINAL NUMBERS: {numbers}")
-    return updated_list
+    # print(f"ORIGINAL NUMBERS: {numbers}")
+    return numbers
 
 def sum_key_values(mixed_list):
     key_values = determine_values_at_key_indices_after_zero(mixed_list)
@@ -40,25 +40,32 @@ def find_index_of_zero(mixed_list):
     index = mixed_list.index(0)
     return index
 
-def move_element(value, index, list):
-    new_index = value + index
-    del list[index]
-    list.insert(new_index, value)
+def move_element(value, current_index, original_index, list):
+    new_index = value + current_index
+    updated_details = {
+        "original_index": original_index,
+        "value": value
+    }
+    del list[current_index]
+    list.insert(new_index, updated_details)
     return list
 
-def copy_original_list(original_list):
-    copy = []
-    for element in original_list:
-        copy.append(element)
-    return copy
+def find_current_index_of_value_based_on_original_index(original_index, list):
+    for i in range(len(list)):
+        if list[i]["original_index"] == original_index:
+            return i
 
 def list_all_numbers(data):
     string_numbers = data.split("\n")
-    integer_numbers = []
-    for string in string_numbers:
-        integer = int(string)
-        integer_numbers.append(integer)
-    return integer_numbers
+    numbers_data = []
+    for i in range(len(string_numbers)):
+        integer = int(string_numbers[i])
+        details = {
+            "original_index": i,
+            "value": integer
+        }
+        numbers_data.append(details)
+    return numbers_data
 
 def extract_data_from_file(day_number):
     file = open(f"day{day_number}/data.txt", "r")
