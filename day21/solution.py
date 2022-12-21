cf55d39cd743691669a62bf4ae496a72d5223560
jtreeves <jackson.t.reeves@gmail.com>
2022-12-21 15:34:49 -0500

    [21] Create Monkey and Description classes

--- day21/solution.py
+++ day21/solution.py
@@ -1,3 +1,50 @@
+class Description:
+    def __init__(self, text):
+        self.text = text
+        self.separate_parts()
+        self.extract_name()
+        self.extract_value_and_expression()
+        self.extract_operation_and_dependencies()
+    
+    def separate_parts(self):
+        parts = self.text.split(": ")
+        self.parts = parts
+    
+    def extract_name(self):
+        name = self.parts[0]
+        self.name = name
+    
+    def extract_value_and_expression(self):
+        value_or_expression = self.parts[1]
+        try:
+            result = int(value_or_expression)
+            self.value = result
+            self.expression = None
+            self.is_waiting = False
+        except ValueError:
+            result = value_or_expression
+            self.expression = result
+            self.value = None
+            self.is_waiting = True
+
+    def extract_operation_and_dependencies(self):
+        if self.expression is not None:
+            divided_expression = self.expression.split(" ")
+            self.operation = divided_expression[1]
+            self.dependencies = [divided_expression[0], divided_expression[2]]
+        else:
+            self.operation = None
+            self.dependencies = []
+
+class Monkey:
+    def __init__(self, text):
+        description = Description(text)
+        self.name = description.name
+        self.value = description.value
+        self.operation = description.operation
+        self.dependencies = description.dependencies
+        self.is_waiting = description.is_waiting
+
 def solve_problem():
     data = extract_data_from_file(21, False)
     return data
