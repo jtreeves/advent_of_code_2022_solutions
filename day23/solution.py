fae14e8330e456947adce8454a684ebef229aaf1
jtreeves <jackson.t.reeves@gmail.com>
2022-12-23 11:05:07 -0500

    [23] Fill in basics

---
+++ day23/solution.py
@@ -0,0 +1,16 @@
+def solve_problem():
+    data = extract_data_from_file(23, False)
+    return data
+
+def extract_data_from_file(day_number, is_official):
+    if is_official:
+        name = "data"
+    else:
+        name = "practice"
+    file = open(f"day{day_number}/{name}.txt", "r")
+    data = file.read()
+    file.close()
+    return data
+
+result = solve_problem()
+print(result)
