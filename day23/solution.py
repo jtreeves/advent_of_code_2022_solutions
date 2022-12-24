8241ac75a86f350108e95c87ce2134d7642b48ea
jtreeves <jackson.t.reeves@gmail.com>
2022-12-24 16:35:34 -0500

    [23] Create pairs

--- day23/solution.py
+++ day23/solution.py
@@ -2,8 +2,8 @@ class CoordinatePair:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.proposed_x = 0
-        self.proposed_y = 0
+        self.proposed_x = None
+        self.proposed_y = None
     
     def __repr__(self):
         return f"({self.x}, {self.y})"
@@ -24,11 +24,24 @@ class CoordinatePair:
 
 class Grid:
     def __init__(self, description):
-        self.rows = description.split("\n")
+        self.lines = description.split("\n")
+        self.original_height = len(self.lines)
+        self.original_width = len(self.lines[0])
+        self.pairs = self.create_pairs()
+    
+    def create_pairs(self):
+        pairs = []
+        for row in range(self.original_height):
+            for column in range(self.original_width):
+                if self.lines[row][column] == "#":
+                    new_pair = CoordinatePair(column, row)
+                    pairs.append(new_pair)
+        return pairs
 
 def solve_problem():
     data = extract_data_from_file(23, False)
-    return data
+    grid = Grid(data)
+    return grid.pairs
 
 def extract_data_from_file(day_number, is_official):
     if is_official:
