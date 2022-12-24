6ccb09d9c04ed5fe9f6042888b6f029c036408b7
jtreeves <jackson.t.reeves@gmail.com>
2022-12-24 16:20:30 -0500

    [23] Create CoordinatePair and Grid classes

--- day23/solution.py
+++ day23/solution.py
@@ -1,3 +1,31 @@
+class CoordinatePair:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.proposed_x = 0
+        self.proposed_y = 0
+    
+    def __repr__(self):
+        return f"({self.x}, {self.y})"
+
+    def check_if_equal(self, other_point):
+        if self.x == other_point.x and self.y == other_point.y:
+            return True
+        else:
+            return False
+
+    def propose_new_location(self, new_x, new_y):
+        self.proposed_x = new_x
+        self.proposed_y = new_y
+
+    def move_to_new_location(self):
+        self.x = self.proposed_x
+        self.y = self.proposed_y
+
+class Grid:
+    def __init__(self, description):
+        self.rows = description.split("\n")
+
 def solve_problem():
     data = extract_data_from_file(23, False)
     return data
