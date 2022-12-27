3c816b38b5af968d68abe44434b1d898952a4830
jtreeves <jackson.t.reeves@gmail.com>
2022-12-26 21:34:56 -0500

    [12] Move conversion function into cell class; add solve problem and extract data helpers

--- day12/solution.py
+++ day12/solution.py
@@ -1,8 +1,17 @@
 class Cell:
-    def __init__(self, x, y, value):
+    def __init__(self, x, y, letter):
         self.x = x
         self.y = y
-        self.value = value
+        self.letter = letter
+        self.value = self.convert_letter_to_number()
+
+    def convert_letter_to_number(self):
+        if self.letter == "S":
+            return 1
+        elif self.letter == "E":
+            return 26
+        else:
+            return ord(self.letter) - 96
 
 class Grid:
     def __init__(self, description):
@@ -16,13 +25,19 @@ class Grid:
     def calculate_width(self):
         return len(self.description[0])
 
-def convert_letter_to_number(letter):
-    if letter == "S":
-        return 1
-    elif letter == "E":
-        return 26
-    else:
-        return ord(letter) - 96
+def solve_problem():
+    data = extract_data_from_file(12, False)
+    return data
 
-print(convert_letter_to_number('a'))
-print(convert_letter_to_number('u'))
+def extract_data_from_file(day_number, is_official):
+    if is_official:
+        name = "data"
+    else:
+        name = "practice"
+    file = open(f"day{day_number}/{name}.txt", "r")
+    data = file.read()
+    file.close()
+    return data
+
+result = solve_problem()
+print(result)
