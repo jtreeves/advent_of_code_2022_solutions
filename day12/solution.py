260d192b8b1b934b79dcc290b5d9c49f337c3527
jtreeves <jackson.t.reeves@gmail.com>
2022-12-26 22:29:28 -0500

    [12] Track total moves; increment on move

--- day12/solution.py
+++ day12/solution.py
@@ -44,6 +44,10 @@ class Traveler:
         self.starting_position = self.find_starting_position()
         self.ending_position = self.find_ending_position()
         self.current_position = self.starting_position
+        self.total_moves = 0
+    
+    def __repr__(self):
+        return f"{self.current_position} -> {self.total_moves} MOVES"
 
     def find_starting_position(self):
         for cell in self.grid.cells:
@@ -71,6 +75,9 @@ class Traveler:
                 return False
         else:
             return False
+
+    def increment_moves(self):
+        self.total_moves += 1
 
     def move_up(self):
         new_x = self.current_position.x
@@ -115,12 +122,16 @@ class Traveler:
             if result == False:
                 continue
             else:
+                self.increment_moves()
                 break
 
 def solve_problem():
     data = extract_data_from_file(12, False)
     grid = Grid(data)
     traveler = Traveler(grid)
+    print(f"BEFORE MOVE: {traveler}")
+    traveler.make_move()
+    print(f"AFTER MOVE: {traveler}")
     return grid.cells
 
 def extract_data_from_file(day_number, is_official):
