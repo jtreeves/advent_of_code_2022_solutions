f73474b737fdac7e021626968ad18b2eef29c3aa
jtreeves <jackson.t.reeves@gmail.com>
2022-12-27 23:38:27 -0500

    [16] Add flow rate and tunnels as properties of Valve

--- day16/solution.py
+++ day16/solution.py
@@ -1,12 +1,38 @@
 class Valve:
     def __init__(self, description):
-        self.name = description[6:8]
+        self.description = description
+        self.name = self.extract_name()
+        self.flow_rate = self.extract_flow_rate()
+        self.tunnels = self.extract_tunnels()
+    
+    def __repr__(self):
+        return f"{self.name}: {self.flow_rate} -> {self.tunnels}"
+    
+    def extract_name(self):
+        name = self.description[6:8]
+        return name
+    
+    def extract_flow_rate(self):
+        index_equal = self.description.find("=")
+        index_semicolon = self.description.find(";")
+        flow_rate = int(self.description[index_equal + 1:index_semicolon])
+        return flow_rate
+    
+    def extract_tunnels(self):
+        partitioned_description = self.description.split(";")
+        tunnels_half = partitioned_description[1]
+        tunnels = tunnels_half.split(", ")
+        tunnels[0] = tunnels[0][-2:]
+        return tunnels
 
 def solve_problem():
     data = extract_data_from_file(16, False)
     lines = data.split("\n")
-    first_valve = Valve(lines[0])
-    return first_valve.name
+    valves = []
+    for line in lines:
+        valve = Valve(line)
+        valves.append(valve)
+    return valves
 
 def extract_data_from_file(day_number, is_official):
     if is_official:
