7943e561f91555a24a1ec5ff8119f0932826c042
jtreeves <jackson.t.reeves@gmail.com>
2022-12-28 20:17:43 -0500

    [22] Create Cell and Board classes to transform input data

--- day22/solution.py
+++ day22/solution.py
@@ -1,6 +1,47 @@
+class Cell:
+    def __init__(self, x, y, character, name):
+        self.x = x
+        self.y = y
+        self.character = character
+        self.name = name
+        self.open = self.check_if_open()
+    
+    def __repr__(self):
+        return f"({self.x}, {self.y}) -> {self.open}"
+    
+    def check_if_open(self):
+        if self.character == ".":
+            return True
+        else:
+            return False
+
+class Board:
+    def __init__(self, description, instructions):
+        self.instructions = instructions
+        self.description = description.split("\n")
+        self.height = len(self.description)
+        self.width = len(self.description[0])
+        self.cells = self.create_cells()
+    
+    def __repr__(self):
+        return f"{self.cells} -> {self.instructions}"
+
+    def create_cells(self):
+        cells = {}
+        for row in range(self.height):
+            for column in range(self.width):
+                character = self.description[row][column]
+                if character != " ":
+                    name = f"x{column}y{row}"
+                    new_cell = Cell(column, row, character, name)
+                    cells[name] = new_cell
+        return cells
+
 def solve_problem():
     data = extract_data_from_file(22, False)
-    return data
+    partitioned = data.split("\n\n")
+    board = Board(partitioned[0], partitioned[1])
+    return board
 
 def extract_data_from_file(day_number, is_official):
     if is_official:
