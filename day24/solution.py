32445e21f7b82cbdd27baf91144625ae1451dd7b
jtreeves <jackson.t.reeves@gmail.com>
2022-12-29 16:02:54 -0500

    [24] Create Blizzard and Traveler classes; create them in Valley at appropriate times in position creation method

--- day24/solution.py
+++ day24/solution.py
@@ -8,12 +8,19 @@ class Position:
         return f"({self.x}, {self.y}): {self.blocked}"
 
 class Blizzard:
-    def __init__(self):
-        pass
+    def __init__(self, position, direction):
+        self.position = position
+        self.direction = direction
+    
+    def __repr__(self):
+        return f"({self.position.x}, {self.position.y}): {self.direction}"
 
 class Traveler:
-    def __init__(self):
-        pass
+    def __init__(self, position):
+        self.position = position
+    
+    def __repr__(self):
+        return f"({self.position.x}, {self.position.y})"
 
 class Valley:
     def __init__(self, description):
@@ -21,6 +28,7 @@ class Valley:
         self.height = len(self.description)
         self.width = len(self.description[0])
         self.positions = self.create_positions()
+        self.blizzards = {}
     
     def __repr__(self):
         return f"{self.starting_position} -> {self.ending_position}"
@@ -36,13 +44,18 @@ class Valley:
                 positions[name] = new_position
                 if row == 0 and not blocked:
                     self.starting_position = name
+                    self.traveler = Traveler(new_position)
                 if row == self.height - 1 and not blocked:
                     self.ending_position = name
+                if character != "#" and character != ".":
+                    self.blizzards[name] = Blizzard(new_position, character)
         return positions
 
 def solve_problem():
     data = extract_data_from_file(24, False)
     valley = Valley(data)
+    print(valley.blizzards)
+    print(valley.traveler)
     return valley
 
 def extract_data_from_file(day_number, is_official):
