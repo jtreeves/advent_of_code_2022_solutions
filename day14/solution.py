12d04c861e9b4859c5dadb7828ade5c94562ffb7
jtreeves <jackson.t.reeves@gmail.com>
2023-01-07 15:31:21 -0500

    [14] Create outline classes for Sand, Path, and Cave

--- day14/solution.py
+++ day14/solution.py
@@ -1,13 +1,25 @@
+class Sand:
+    def __init__(self):
+        pass
+
+class Path:
+    def __init__(self):
+        pass
+
+class Cave:
+    def __init__(self, description):
+        self.descriptions = description.split("\n")
+
 def solve_problem():
-    data = extract_data_from_file(14)
+    data = extract_data_from_file(14, False)
     return data
 
-def list_all_paths(data):
-    partitioned = data.split("\n")
-    return partitioned
-
-def extract_data_from_file(day_number):
-    file = open(f"day{day_number}/data.txt", "r")
+def extract_data_from_file(day_number, is_official):
+    if is_official:
+        name = "data"
+    else:
+        name = "practice"
+    file = open(f"day{day_number}/{name}.txt", "r")
     data = file.read()
     file.close()
     return data
