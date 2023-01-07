325998a0fb81b9f9dcb35df833a23aa3ff2a0195
jtreeves <jackson.t.reeves@gmail.com>
2023-01-07 16:20:09 -0500

    [14] Add eq and hash methods to Point class to allow for comparing in sets

--- day14/solution.py
+++ day14/solution.py
@@ -5,6 +5,18 @@ class Point:
 
     def __repr__(self):
         return f"({self.x}, {self.y})"
+
+    def __eq__(self, other):
+        if isinstance(other, Point):
+            if self.x == other.x and self.y == other.y:
+                return True
+            else:
+                return False
+        else:
+            return False
+    
+    def __hash__(self):
+        return hash((self.x, self.y))
     
     def has_identical_x(self, other_point):
         return self.x == other_point.x
@@ -26,7 +38,7 @@ class Path:
         self.all_points = self.create_all_points()
 
     def __repr__(self):
-        return f"{self.all_points[0]} -> {self.all_points[-1]}: {len(self.all_points)}"
+        return f"{self.all_points}"
 
     def create_anchor_points(self):
         points = []
@@ -37,18 +49,18 @@ class Path:
         return points
 
     def create_all_points(self):
-        points = []
+        points = set()
         for i in range(len(self.anchor_points) - 1):
             matching_x = self.anchor_points[i].has_identical_x(self.anchor_points[i + 1])
             matching_y = self.anchor_points[i].has_identical_y(self.anchor_points[i + 1])
             if matching_x:
                 for y in range(self.anchor_points[i].y, self.anchor_points[i + 1].y):
                     new_point = Point(self.anchor_points[i].x, y)
-                    points.append(new_point)
+                    points.add(new_point)
             elif matching_y:
                 for x in range(self.anchor_points[i + 1].x, self.anchor_points[i].x):
                     new_point = Point(x, self.anchor_points[i].y)
-                    points.append(new_point)
+                    points.add(new_point)
         return points
 
 class Cave:
@@ -69,6 +81,9 @@ class Cave:
 def solve_problem():
     data = extract_data_from_file(14, False)
     cave = Cave(data)
+    for path in cave.paths:
+        for point in path.all_points:
+            print(point)
     return cave
 
 def extract_data_from_file(day_number, is_official):
