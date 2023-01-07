86acf710de53ac531c4d49d7d47a40338148bf3a
jtreeves <jackson.t.reeves@gmail.com>
2023-01-07 16:32:27 -0500

    [14] Add endpoints to path sets; add lt method to point class to allow sorting

--- day14/solution.py
+++ day14/solution.py
@@ -14,6 +14,17 @@ class Point:
                 return False
         else:
             return False
+
+    def __lt__(self, other):
+        if isinstance(other, Point):
+            if self.x < other.x:
+                return True
+            elif self.x == other.x and self.y < other.y:
+                return True
+            else:
+                return False
+        else:
+            return False
     
     def __hash__(self):
         return hash((self.x, self.y))
@@ -38,7 +49,7 @@ class Path:
         self.all_points = self.create_all_points()
 
     def __repr__(self):
-        return f"{self.all_points}"
+        return f"{self.all_points}: {len(self.all_points)}"
 
     def create_anchor_points(self):
         points = []
@@ -51,17 +62,21 @@ class Path:
     def create_all_points(self):
         points = set()
         for i in range(len(self.anchor_points) - 1):
-            matching_x = self.anchor_points[i].has_identical_x(self.anchor_points[i + 1])
-            matching_y = self.anchor_points[i].has_identical_y(self.anchor_points[i + 1])
+            left_endpoint = self.anchor_points[i]
+            right_endpoint = self.anchor_points[i + 1]
+            matching_x = left_endpoint.has_identical_x(right_endpoint)
+            matching_y = left_endpoint.has_identical_y(right_endpoint)
+            points.add(left_endpoint)
+            points.add(right_endpoint)
             if matching_x:
-                for y in range(self.anchor_points[i].y, self.anchor_points[i + 1].y):
-                    new_point = Point(self.anchor_points[i].x, y)
+                for y in range(left_endpoint.y, right_endpoint.y):
+                    new_point = Point(left_endpoint.x, y)
                     points.add(new_point)
             elif matching_y:
-                for x in range(self.anchor_points[i + 1].x, self.anchor_points[i].x):
-                    new_point = Point(x, self.anchor_points[i].y)
+                for x in range(right_endpoint.x, left_endpoint.x):
+                    new_point = Point(x, left_endpoint.y)
                     points.add(new_point)
-        return points
+        return sorted(points)
 
 class Cave:
     def __init__(self, description):
@@ -81,9 +96,6 @@ class Cave:
 def solve_problem():
     data = extract_data_from_file(14, False)
     cave = Cave(data)
-    for path in cave.paths:
-        for point in path.all_points:
-            print(point)
     return cave
 
 def extract_data_from_file(day_number, is_official):
