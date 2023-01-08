a5e5ef3157d22e4b8602e06103374e46d1846f15
jtreeves <jackson.t.reeves@gmail.com>
2023-01-07 21:55:36 -0500

    [15] List all beacons and beaconless positions

--- day15/solution.py
+++ day15/solution.py
@@ -117,13 +117,19 @@ class Grid:
             all_positions.extend(row_positions)
         return all_positions
 
+    def list_all_beacons_and_beaconless_positions(self, maximum_row):
+        beacon_positions = self.beacons
+        beaconless_positions = self.find_all_beaconless_positions(maximum_row)
+        all_positions = beacon_positions + beaconless_positions
+        return all_positions
+
     def find_only_position_for_missing_beacon(self):
         return
 
 def solve_problem():
     data = extract_data_from_file(15, False)
     grid = Grid(data)
-    positions = grid.find_all_beaconless_positions(20)
+    positions = grid.list_all_beacons_and_beaconless_positions(20)
     return positions
 
 def extract_data_from_file(day_number, is_official):
