0b6e28a57b4552726743c2fa915b12bc388c459a
jtreeves <jackson.t.reeves@gmail.com>
2023-01-08 00:21:42 -0500

    [19] Add checkers to blueprint class to see if can make robots; add method to spend a minute; add method to spend multiple minutes

--- day19/solution.py
+++ day19/solution.py
@@ -44,6 +44,14 @@ class Blueprint:
         self.id = self.extract_id()
         self.robot_descriptions = self.descriptions[1].split(". ")
         self.robot_specs = self.determine_all_robot_specs()
+        self.ore_robots = 1
+        self.clay_robots = 0
+        self.obsidian_robots = 0
+        self.geode_robots = 0
+        self.ore = 0
+        self.clay = 0
+        self.obsidian = 0
+        self.geodes = 0
     
     def __repr__(self):
         description = f"{self.id}: "
@@ -63,6 +71,76 @@ class Blueprint:
             new_spec = Robot(description)
             specs.append(new_spec)
         return specs
+    
+    def check_if_can_make_geode_robot(self):
+        geode_requirements = self.robot_specs[3].requirements
+        ore_needed = geode_requirements[0].amount
+        obsidian_needed = geode_requirements[1].amount
+        if self.ore >= ore_needed and self.obsidian >= obsidian_needed:
+            return True
+        else:
+            return False
+    
+    def check_if_can_make_obsidian_robot(self):
+        obsidian_requirements = self.robot_specs[2].requirements
+        ore_needed = obsidian_requirements[0].amount
+        clay_needed = obsidian_requirements[1].amount
+        if self.ore >= ore_needed and self.clay >= clay_needed:
+            return True
+        else:
+            return False
+    
+    def check_if_can_make_clay_robot(self):
+        clay_requirements = self.robot_specs[2].requirements
+        ore_needed = clay_requirements[0].amount
+        if self.ore >= ore_needed:
+            return True
+        else:
+            return False
+        
+    def spend_one_minute(self):
+        can_geode = self.check_if_can_make_geode_robot()
+        can_obsidian = self.check_if_can_make_obsidian_robot()
+        can_clay = self.check_if_can_make_clay_robot()
+        if can_geode:
+            geode_requirements = self.robot_specs[3].requirements
+            ore_needed = geode_requirements[0].amount
+            obsidian_needed = geode_requirements[1].amount
+            self.ore -= ore_needed
+            self.obsidian -= obsidian_needed
+        elif can_obsidian:
+            obsidian_requirements = self.robot_specs[2].requirements
+            ore_needed = obsidian_requirements[0].amount
+            clay_needed = obsidian_requirements[1].amount
+            self.ore -= ore_needed
+            self.clay -= clay_needed
+        elif can_clay:
+            clay_requirements = self.robot_specs[2].requirements
+            ore_needed = clay_requirements[0].amount
+            self.ore -= ore_needed
+        for _ in range(self.ore_robots):
+            self.ore += 1
+        for _ in range(self.clay_robots):
+            self.clay += 1
+        for _ in range(self.obsidian_robots):
+            self.obsidian += 1
+        for _ in range(self.geode_robots):
+            self.geodes += 1
+        if can_geode:
+            self.geode_robots += 1
+        elif can_obsidian:
+            self.obsidian_robots += 1
+        elif can_clay:
+            self.clay_robots += 1
+
+    def spend_multiple_minutes(self, minutes):
+        for _ in range(minutes):
+            print(f"MINUTE: {minutes + 1}")
+            self.spend_one_minute()
+            print(f"ORE: {self.ore}")
+            print(f"CLAY: {self.clay}")
+            print(f"OBSIDIAN: {self.obsidian}")
+            print(f"GEODES: {self.geodes}")
 
 class Selection:
     def __init__(self, description):
@@ -86,7 +164,7 @@ class Selection:
 def solve_problem():
     data = extract_data_from_file(19, False)
     selection = Selection(data)
-    return selection
+    selection.blueprints[0].spend_multiple_minutes(5)
 
 def extract_data_from_file(day_number, is_official):
     if is_official:
