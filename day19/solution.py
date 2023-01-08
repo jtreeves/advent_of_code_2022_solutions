516b1679fdb3318acff7dae2537f60f9167722dc
jtreeves <jackson.t.reeves@gmail.com>
2023-01-07 23:12:13 -0500

    [19] Extract cost clay robot

--- day19/solution.py
+++ day19/solution.py
@@ -4,6 +4,7 @@ class Blueprint:
         self.robot_descriptions = self.descriptions[1].split(". ")
         self.id = self.extract_id()
         self.ore_cost = self.extract_cost_ore_robot()
+        self.clay_cost = self.extract_cost_clay_robot()
     
     def __repr__(self):
         return f"{self.id}: {self.ore_cost} ore -> ORE"
@@ -17,6 +18,11 @@ class Blueprint:
         ore_descriptions = self.robot_descriptions[0].split("costs ")
         ore_cost = int(ore_descriptions[1].split(" ")[0])
         return ore_cost
+    
+    def extract_cost_clay_robot(self):
+        clay_descriptions = self.robot_descriptions[1].split("costs ")
+        clay_cost = int(clay_descriptions[1].split(" ")[0])
+        return clay_cost
 
 class Selection:
     def __init__(self, description):
