ba6808f01fef94e3388b6904cb89a6472400d8ff
jtreeves <jackson.t.reeves@gmail.com>
2023-01-16 16:10:47 -0500

    [16] Revert since may be more optimal to skip an openable valve

--- day16/solution.py
+++ day16/solution.py
@@ -144,23 +144,22 @@ class Exploration:
                     opened_valves.add(current_valve.name)
                     opened_state = State(current_valve, pressure, time, visited_valves, opened_valves)
                     queue.append(opened_state)
-                else:
-                    for tunnel in current_state.current_valve.tunnels:
-                        visiting_states_copy = current_state.create_divergent_copy()
-                        current_valve = visiting_states_copy["current_valve"]
-                        pressure = visiting_states_copy["pressure"]
-                        time = visiting_states_copy["time"]
-                        visited_valves = visiting_states_copy["visited_valves"]
-                        opened_valves = visiting_states_copy["opened_valves"]
-                        current_valve = self.find_valve_by_name(tunnel)
-                        time += 1
-                        visited_valves.add(current_valve.name)
-                        visited_state = State(current_valve, pressure, time, visited_valves, opened_valves)
-                        queue.append(visited_state)
+                for tunnel in current_state.current_valve.tunnels:
+                    visiting_states_copy = current_state.create_divergent_copy()
+                    current_valve = visiting_states_copy["current_valve"]
+                    pressure = visiting_states_copy["pressure"]
+                    time = visiting_states_copy["time"]
+                    visited_valves = visiting_states_copy["visited_valves"]
+                    opened_valves = visiting_states_copy["opened_valves"]
+                    current_valve = self.find_valve_by_name(tunnel)
+                    time += 1
+                    visited_valves.add(current_valve.name)
+                    visited_state = State(current_valve, pressure, time, visited_valves, opened_valves)
+                    queue.append(visited_state)
         return max_pressure
 
 def solve_problem():
-    data = extract_data_from_file(16, False)
+    data = extract_data_from_file(16, True)
     experience = Exploration(data)
     max_pressure = experience.find_maximum_pressure(30)
     return max_pressure
